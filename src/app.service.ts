c5a0783cbed1f20d420111df7fa39101d6f3e80b
Vitos <vitos@Macs-MacBook-Air.local>
2024-06-28 18:19:49 +0300

    feat: added clerk integration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import { clerkClient } from '@clerk/clerk-sdk-node';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  getUsers() {
+    return clerkClient.users.getUserList();
   }
 }
