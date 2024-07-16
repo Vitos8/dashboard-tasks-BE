702ea4bde76ab8612825597c14f3443edb3ed7b5
Vitos <vitos@Macs-MacBook-Air.local>
2024-07-16 17:39:50 +0300

    deploy changes

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(process.env.port, '0.0.0.0' );
 }
 bootstrap();
