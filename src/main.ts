4afceea7149f9e5b37cb788a0d72c7860fddbc62
Vitos <vitos@Macs-MacBook-Air.local>
2024-07-17 13:14:57 +0300

    disabled corsss

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import * as process from "process"
 dotenv.config();
+import cors from 'cors'
 
 async function bootstrap() {
   const port = process.env.PORT || 4000
   console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,123321)
   const app = await NestFactory.create(AppModule, { cors: false });
-  app.enableCors();
+  app.use(cors({
+    credentials: true,
+    origin: '*'
+  }))
   app.useGlobalPipes(new ValidationPipe());
   
   await app.listen(port);
