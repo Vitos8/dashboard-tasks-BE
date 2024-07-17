284aaa6b431c25a14765cb5f8e86def56bda2656
Vitos <vitos@Macs-MacBook-Air.local>
2024-07-17 12:39:26 +0300

    deploy changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ClerkAuthGuard } from './auth/clerk.auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  @UseGuards(ClerkAuthGuard)
   getUsers() {
     return this.appService.getUsers();
   }
